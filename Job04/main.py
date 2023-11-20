33b8b45477200c95e9fba3c197073cff6cb41c61
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2023-11-20 14:18:28 +0100

    Job04 et Job05 fini

---
+++ Job04/main.py
@@ -0,0 +1,5 @@
+alphabet_liste = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+print(*alphabet_liste, sep=' ')
+         #_______#
+print('A B C D E F G H I J K L M N O P Q R S T U V W X Y Z')
+                   #_____#
