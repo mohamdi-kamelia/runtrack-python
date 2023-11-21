90a05e56d4548d34ccc1568ccea204531d9273c7
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2023-11-21 17:47:08 +0100

    Travail 02 terminer

--- Jour01/Job05/main.py
+++ Jour01/Job05/main.py
@@ -2,6 +2,6 @@ alphabet_liste = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
 alphabet_liste_inverse = alphabet_liste[::-1]
 print(*alphabet_liste_inverse, sep=' ')
                      #______#
-alphabet_liste = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet_liste = [ 'A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 alphabet_liste_inverse = list(reversed(alphabet_liste))
 print(*alphabet_liste_inverse, sep=' ')
